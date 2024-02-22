b1dcd80aabfe2475b620090e136cc82aa9a4968d
klement97 <klementomeri97@gmail.com>
2024-02-23 00:43:02 +0100

    feat: add repo ports to be used as interfaces

--- src/domain/ports/repo.py
+++ src/domain/ports/repo.py
@@ -1,16 +1,30 @@
+import abc
 from abc import ABC
 
 from elasticsearch import AsyncElasticsearch
 
+from src.domain.entities.auth import User
+
 
 class RepoPort(ABC):
+    users_index = "users"
     vehicles_index = 'vehicles'
 
     def __init__(self, client: AsyncElasticsearch):
         self.client = client
 
 
-class InsertRepoPort(RepoPort):
+class InsertRepoPort(RepoPort, abc.ABC):
+    @abc.abstractmethod
+    async def insert(self, entity):
+        ...
 
-    def insert(self, entity):
-        raise NotImplementedError()
+
+class AuthRepoPort(InsertRepoPort, abc.ABC):
+    @abc.abstractmethod
+    async def insert(self, user: User):
+        ...
+
+    @abc.abstractmethod
+    async def get(self, email: str) -> User:
+        ...
