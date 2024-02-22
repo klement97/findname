82314636ccbe47c56b1ec9329c75a9461181ff23
klement97 <klementomeri97@gmail.com>
2024-02-23 00:42:09 +0100

    feat: add domain exceptions for auth

--- src/domain/exceptions.py
+++ src/domain/exceptions.py
@@ -1,15 +1,30 @@
-class BaseError(Exception):
+class _BaseException(Exception):
     def __init__(self, message):
         self.message = message
 
 
-class PublicationExistsError(BaseError):
+class PublicationExistsException(_BaseException):
     def __init__(self, publication_id, message="Publication already exists"):
         super().__init__(message)
         self.publication_id = publication_id
 
 
-class PublicationError(BaseError):
+class PublicationException(_BaseException):
     def __init__(self, publication_id, message="Publication already exists"):
         super().__init__(message)
         self.publication_id = publication_id
+
+
+class UserDoesNotExist(_BaseException):
+    def __init__(self, message="User does not exist!"):
+        super().__init__(message)
+
+
+class UserAlreadyExistsException(_BaseException):
+    def __init__(self, message="User already exists!"):
+        super().__init__(message)
+
+
+class PasswordIncorrectException(_BaseException):
+    def __init__(self, message="Email or Password is incorrect!"):
+        super().__init__(message)
