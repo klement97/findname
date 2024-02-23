0404f8a8f60e7c53effd5d7fd2c333b25ae465f5
klement97 <klementomeri97@gmail.com>
2024-02-23 15:15:29 +0100

    refactor: rename deps

--- src/infra/dependencies.py
+++ src/infra/dependencies.py
@@ -1,10 +1,39 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import status
+from jose import jwt, JWTError
+
 from src.adapters.repos.auth import AuthRepo
 from src.adapters.repos.vehicle import VehicleRepo
 from src.domain.use_cases.login import LoginUseCase
 from src.domain.use_cases.publish_vehicle import PublishVehicleUseCase
+from src.domain.use_cases.read_user_info import ReadUserInfoUseCase
 from src.domain.use_cases.signup import SignupUseCase
 from src.infra import config
 from src.infra.db import Database, AiohttpHttpNode
+
+# Using an additional /auth/login-form endpoint so we can use the class directly.
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login-form")
+
+
+async def get_current_user_email(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    return email
 
 
 async def get_db():
@@ -23,26 +52,31 @@ async def get_db():
     )
 
 
-async def get_vehicle_repo():
+async def vehicle_repo():
     db = await get_db()
     return VehicleRepo(client=db.client)
 
 
-async def get_auth_repo():
+async def auth_repo():
     db = await get_db()
     return AuthRepo(db.client)
 
 
-async def get_signup_use_case():
-    repo = await get_auth_repo()
+async def signup_use_case():
+    repo = await auth_repo()
     return SignupUseCase(repo)
 
 
-async def get_login_use_case():
-    repo = await get_auth_repo()
+async def login_use_case():
+    repo = await auth_repo()
     return LoginUseCase(repo)
 
 
-async def get_publish_vehicle_use_case():
-    repo = await get_vehicle_repo()
+async def read_user_info_use_case():
+    repo = await auth_repo()
+    return ReadUserInfoUseCase(repo)
+
+
+async def publish_vehicle_use_case():
+    repo = await vehicle_repo()
     return PublishVehicleUseCase(repo)
--- src/infra/routers/vehicle.py
+++ src/infra/routers/vehicle.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from src.domain.entities.vehicle_publication import VehiclePublication
 from src.domain.use_cases.publish_vehicle import PublishVehicleUseCase
-from src.infra.dependencies import get_publish_vehicle_use_case
+from src.infra.dependencies import publish_vehicle_use_case
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ router = APIRouter()
 )
 async def publish(
         vehicle_model: VehiclePublication,
-        publish_vehicle_use_case: PublishVehicleUseCase = Depends(get_publish_vehicle_use_case),
+        use_case: PublishVehicleUseCase = Depends(publish_vehicle_use_case),
 ):
-    await publish_vehicle_use_case.execute(vehicle_model)
+    await use_case.execute(vehicle_model)
     return {"message": "Vehicle published successfully"}
