3a0bc5593ac9b3fdff975566392f717f997ffd71
klement97 <klementomeri97@gmail.com>
2024-02-24 02:04:02 +0100

    refactor: repo ports

--- src/domain/ports/repo.py
+++ src/domain/ports/repo.py
@@ -8,23 +8,20 @@ from src.domain.entities.auth import User
 
 class RepoPort(ABC):
     users_index = "users"
-    vehicles_index = 'vehicles'
+    vehicles_index = "vehicles"
 
-    def __init__(self, client: AsyncElasticsearch):
+    def __init__(self, client):
         self.client = client
 
 
 class InsertRepoPort(RepoPort, abc.ABC):
     @abc.abstractmethod
-    async def insert(self, entity):
-        ...
+    async def insert(self, entity): ...
 
 
 class AuthRepoPort(InsertRepoPort, abc.ABC):
     @abc.abstractmethod
-    async def insert(self, user: User):
-        ...
+    async def insert(self, user: User): ...
 
     @abc.abstractmethod
-    async def get(self, email: str) -> User:
-        ...
+    async def get(self, email: str) -> User: ...
