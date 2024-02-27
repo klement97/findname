82daf4e9b464efff769f93d9cf9038caa63e8839
klement97 <klementomeri97@gmail.com>
2024-02-28 00:13:35 +0100

    feat(CQRS): implement vehicle write

--- src/infra/routers/write/vehicle.py
+++ src/infra/routers/write/vehicle.py
@@ -1,17 +1,21 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 
 from src.domain.entities.vehicle_publication import VehiclePublication
 from src.domain.use_cases.vehicle.publish_vehicle import PublishVehicleUseCase
+from src.infra import dependencies as deps
 from src.infra.dependencies import publish_vehicle_use_case
 
 router = APIRouter()
 
 
 @router.post(
-    "/publish",
+    "/publication",
     response_model=dict[str, str],
 )
 async def publish(
+        _: Annotated[str, Depends(deps.oauth2_scheme)],
         vehicle_model: VehiclePublication,
         use_case: PublishVehicleUseCase = Depends(publish_vehicle_use_case),
 ):
