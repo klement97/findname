03670d192875903992879adeb101756e77324688
klement97 <klementomeri97@gmail.com>
2024-02-28 00:12:09 +0100

    refactor: use VehicleRepoPort

--- src/domain/use_cases/vehicle/publish_vehicle.py
+++ src/domain/use_cases/vehicle/publish_vehicle.py
@@ -1,11 +1,11 @@
 from src.domain.entities.vehicle_publication import VehiclePublication
-from src.domain.ports.repo import InsertRepoPort
+from src.domain.ports.repo import VehicleRepoPort
 from src.domain.ports.use_case import UseCasePort
 
 
 class PublishVehicleUseCase(UseCasePort):
 
-    def __init__(self, vehicle_repo: InsertRepoPort):
+    def __init__(self, vehicle_repo: VehicleRepoPort):
         self.vehicle_repo = vehicle_repo
 
     async def execute(self, vp: VehiclePublication):
